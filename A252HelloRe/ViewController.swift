b4a6ad0b03fc1c610a9749dc5a5655fb0ed959c3
Danny Shen <shenfive@gmail.com>
2025-03-09 16:06:50 +0800

    no message

--- A252HelloRe/ViewController.swift
+++ A252HelloRe/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+ 
+    @IBOutlet weak var theIndicator: UIActivityIndicatorView!
+    var timer:Timer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { _ in
+            self.getNetWorkStatus()
+        })
     }
 
+    func getNetWorkStatus(){
+        if Reachability(hostName: "www.apple.com")?
+            .currentReachabilityStatus().rawValue == 0 {
+            theIndicator.startAnimating()
+        }else{
+            theIndicator.stopAnimating()
+        }
+    }
 
 }
 
